215dfa21ca867d29b097db01804731de49d861f5
CaioBonvicine <caiopbonvicine@gmail.com>
2026-08-29 12:01:42 -0300

    fix: fixing the code not deleverying nothing

--- run.sh
+++ run.sh
@@ -5,9 +5,10 @@ sudo docker build -t agente-epi .
 echo "Iniciando o container..."
 # --privileged: necessário para acessar o /sys/class/gpio do servo motor
 # -v /dev/video0:/dev/video0: passa a câmera USB para dentro do container
-sudo docker run -it --rm \
+sudo docker run \
     --runtime nvidia \
     --privileged \
     -v /dev/video0:/dev/video0 \
     --network host \
     agente-epi
+echo "Exit code: $?"
